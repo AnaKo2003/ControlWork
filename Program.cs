da1f7469ffaa9678bdb1d409ea4f2bc71d0a3dd4
ANKO <pannikovaanna2000@mail.ru>
2022-07-29 13:03:20 +0300

    Первый метод

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿string[] inputArray = { "hello", "2", "world", ":-)"};
 string[] resultArray = new string[inputArray.Length];
+
+void ArraysOfStrings(string[] inputArray, string[] resultArray)
+{
+    int count = 0;
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+      if(inputArray[i].Length <= 3)
+        {
+        resultArray[count] = inputArray[i];
+        count++;
+        }
+    }
+}
